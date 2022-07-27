66a5fd279a31d5a14db5bfe546b2de97956fec8f
PlayerG9 <68517540+PlayerG9@users.noreply.github.com>
2022-07-27 21:08:58 +0200

    setup the multipart download

--- src/web-ui/src/App/index.tsx
+++ src/web-ui/src/App/index.tsx
@@ -1,8 +1,13 @@
 import './style.scss'
+import './Components/style.scss'
+import { NotificationsDisplay } from './Components/notification'
+import DownloadInterface from './DownloadInterface'
 
 
 export default function App() {
-  return <>
-    Hello World
-  </>
+	return <div className='app'>
+		<h1>mp3music</h1>
+		<DownloadInterface/>
+		<NotificationsDisplay/>
+	</div>
 }
