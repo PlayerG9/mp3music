495d0e84f654fb774c40d82a901e540bca577ab7
PlayerG9 <68517540+PlayerG9@users.noreply.github.com>
2022-07-27 21:07:56 +0200

    added the websocket endpoint

--- src/web-ui/src/App/apiCommunication/index.ts
+++ src/web-ui/src/App/apiCommunication/index.ts
@@ -4,3 +4,7 @@ export const SERVERADDRESS = 'mp3music-backend.herokuapp.com'
 export function getAudioDownloadLink(uid: string){
     return `https://${SERVERADDRESS}/api/mp3file/${uid}`
 }
+
+export function getDownloadWebsocketUrl(){
+    return `ws://${SERVERADDRESS}/api/download`
+}
