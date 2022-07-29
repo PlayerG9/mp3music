a963a50a88a9e5b1a64b515dc155e3cc756b2aa0
PlayerG9 <68517540+PlayerG9@users.noreply.github.com>
2022-07-29 20:08:02 +0200

    improved response type

--- src/web-ui/src/App/apiCommunication/types.ts
+++ src/web-ui/src/App/apiCommunication/types.ts
@@ -1,3 +1,9 @@
 export interface VideoMetadata {
-
+    video_id: string,
+    title: string,
+    author: string,
+    description: string,
+    channel_id: string,
+    thumbnail_url: string,
+    views: number
 }
