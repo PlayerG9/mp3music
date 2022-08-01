c1f808936e3f1a74a4c58130e713ff31c99aff61
PlayerG9 <68517540+PlayerG9@users.noreply.github.com>
2022-08-01 16:17:31 +0200

    added style for the messages

--- src/web-ui/src/App/DownloadInterface/components/MessageRenderer.tsx
+++ src/web-ui/src/App/DownloadInterface/components/MessageRenderer.tsx
@@ -3,15 +3,15 @@ import { DownloadProtocolMessage } from "../typescriptData"
 
 export default function MessageRenderer(msg: DownloadProtocolMessage){
     if(msg.info){
-        return <div className='info'>
+        return <div className='message info'>
             {msg.info}
         </div>
     }else if(msg.warning){
-        return <div className='warning'>
+        return <div className='message warning'>
             {msg.warning}
         </div>
     }else if(msg.error){
-        return <div className='error'>
+        return <div className='message error'>
             {msg.error_class}: {msg.error}
         </div>
     }else{
